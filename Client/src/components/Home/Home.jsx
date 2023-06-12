3b55c27ce7ff31c62eddc19087cb9a2b56a43121
Valkiria5 <dharmavaninaramayoquilinan@gmail.com>
2023-06-12 13:40:47 -0300

    se hizo card y cardcontainer, y renderizo a home

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import CardContainer from "../CardContainer/CardContainer";
 const Home = () => {
   return (
     <div>
       <h1> Esta es nuestra Home</h1>
+      <CardContainer/>
     </div>
   );
 };
