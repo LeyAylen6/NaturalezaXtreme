8a974225034b6b09601898f1769930fcc02cab32
Leila <leiisalguero@gmaill.com>
2023-06-12 17:10:52 -0300

    modify db  conection

--- Server/src/db.ts
+++ Server/src/db.ts
@@ -1,14 +1,15 @@
 require('dotenv').config();
 const { User } = require('./entities/userEntity')
 import "reflect-metadata"
+const { PASSWORD, USERNAME, DB_HOST } = process.env
 import { DataSource } from "typeorm"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: DB_HOST,
     port: 5432,
-    username: "postgres",
-    password: "070118",
+    username: USERNAME,
+    password: PASSWORD,
     database: "NaturalezaXtreme",
     synchronize: true,
     logging: false,
