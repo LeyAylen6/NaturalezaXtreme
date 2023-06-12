c03313fa7d760225ce3d5c524be9d7dba8dc5ba7
Franco <francofran1998@gmail.com>
2023-06-12 19:50:40 -0300

    cambios

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import CardContainer from "../CardContainer/CardContainer";
 import Filters from "../Filters/Filters";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const Home = () => {
   return (
-    <div>
+    <ChakraProvider>
       <Filters />
       <CardContainer />
-    </div>
+    </ChakraProvider>
   );
 };
 
