1543109a9c64d84a973d8f8037c8add1a170b96c
Valkiria5 <dharmavaninaramayoquilinan@gmail.com>
2023-06-12 14:02:32 -0300

    proyecto de AboutUs

---
+++ Client/src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,29 @@
+const AboutUs = () => {
+    return (
+        <div>
+            <h1>¡Bienvenido!</h1>
+            <p> Bienvenido a nuestra tienda en línea, tu destino ideal para encontrar todo lo que necesitas para tus 
+                aventuras al aire libre. Nos enorgullece ofrecerte una amplia gama de artículos
+                e indumentaria especializada para actividades emocionantes como trekking, escalada y mucho más.</p>
+
+            <h2>¿Quienes somos?</h2>
+
+            <p>Nosotros somos estudiantes de Henry que, luego de pasar por las distintas etapas del bootcapm, han llego a esta etapa final. 
+              * Leslie Quetglas
+              * Agustin Diaz
+              * Franco Lionel Rodriguez Caserta
+              * Julieta Gimenez
+              * Leila Salguero
+              * Sebastian Concheso
+              * Nicolas Florentin
+              * Vanina Ramayo
+            </p>
+
+            <h2>¿Que es NatureXtreme?</h2>
+
+            <p>NatureXtreme es un proyecto realizado para pulir las bases del trabajo en equipo, es un e-commerce enfocado en actividades al aire libre
+            </p>
+            
+        </div>
+    )
+}
