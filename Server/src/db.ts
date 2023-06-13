2071ae1d40f8a6ffdca7bd76e3888ea25a937248
Leila <leiisalguero@gmaill.com>
2023-06-13 15:00:24 -0300

    add relation user-shoppingCart and bug fixes in dev

--- Server/src/db.ts
+++ Server/src/db.ts
@@ -1,7 +1,8 @@
 require('dotenv').config();
-const { User } = require('./entities/userEntity')
-const { Article } = require('./entities/articleEntity')
+import { User } from './entities/userEntity';
+import { Article } from './entities/articleEntity';
 import { Post } from "./entities/postEntity";
+import { Shopping_Cart } from "./entities/shoppingCartEntity";
 import "reflect-metadata"
 const { DB_PORT, DB_PASSWORD, DB_USER, DB_HOST, DB_NAME_PROJECT } = process.env
 import { DataSource } from "typeorm"
@@ -15,7 +16,7 @@ export const AppDataSource = new DataSource({
     database: DB_NAME_PROJECT,
     synchronize: true,
     logging: false,
-    entities: [User, Article, Post],
+    entities: [User, Article, Post, Shopping_Cart],
     subscribers: [],
     migrations: [],
 })
