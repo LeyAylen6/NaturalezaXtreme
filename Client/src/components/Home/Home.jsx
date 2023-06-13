a8650ad809ae1ef04080852eb881c1a7445949f2
Franco <francofran1998@gmail.com>
2023-06-13 13:49:02 -0300

    Add some styles to Nav Bar

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import CardContainer from "../CardContainer/CardContainer";
 import Filters from "../Filters/Filters";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
 const Home = () => {
   return (
-    <ChakraProvider>
+    <Box>
       <Filters />
       <CardContainer />
-    </ChakraProvider>
+    </Box>
   );
 };
 
--- Client/src/components/NavBar/NavBar.jsx
+++ Client/src/components/NavBar/NavBar.jsx
@@ -1,21 +1,50 @@
 import { Link } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 import Styles from "./NavBar.module.css";
+import { Box, Stack, HStack } from "@chakra-ui/react";
 
 const NavBar = () => {
   return (
-    <nav className={Styles.nav}>
-      <Link to="/">Products</Link>
-      <Link to="/aboutus">About Us</Link>
-
-      <div>
-        <SearchBar />
-      </div>
-      <Link to="/favorite">Favorite</Link>
-      <Link to="/cart">Cart</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/signUp">Sign Up</Link>
-    </nav>
+    <Box>
+      <Stack
+        direction={"row"}
+        width="1280px"
+        bgGradient="linear(to-r,#16141c,#6c6f78)"
+        spacing={100}
+        padding={"10px"}
+        alignItems={"center"}
+        fontSize={"20px"}
+      >
+        <Box
+          bg={"transparent"}
+          marginLeft={"10px"}
+          border={"none"}
+          color={"white"}
+        >
+          <Link to="/">Products</Link>
+        </Box>
+        <Box bg={"transparent"} color={"white"}>
+          <Link to="/aboutus">About</Link>
+        </Box>
+        <Box bg={"transparent"} color={"white"}>
+          <Link to="/favorite">Favorite</Link>
+        </Box>
+        <Box bg={"transparent"} color={"white"}>
+          <Link to="/cart">Cart</Link>
+        </Box>
+        <Box bg={"transparent"} color={"white"}>
+          <SearchBar />
+        </Box>
+        <HStack>
+          <Box bg={"transparent"} color={"white"}>
+            <Link to="/login">Login</Link>
+          </Box>
+          <Box bg={"transparent"} color={"white"}>
+            <Link to="/signUp">Sign Up</Link>
+          </Box>
+        </HStack>
+      </Stack>
+    </Box>
   );
 };
 
