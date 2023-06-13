6089bfb9dcad1664d17e06a84fac5e30246535a6
unknown <agustin87_7@hotmail.com>
2023-06-13 18:22:03 -0300

    add shoppCartArticle

---
+++ Server/src/entities/shoppingCartArticleEntity.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column,ManyToOne } from 'typeorm'
+import { Shopping_Cart } from './shoppingCartEntity'
+import { Article } from './articleEntity' 
+
+@Entity()
+export class Shopping_Cart_Article {
+    
+    @PrimaryGeneratedColumn()
+    public id: number
+
+    @Column()
+    public quantily: number
+
+
+
+    @ManyToOne(() => Shopping_Cart, (shoppingCart) => shoppingCart.shoppingArticles)
+    public shoppingCarts: Shopping_Cart
+
+    @ManyToOne(() => Article, (article) => article.shoppingArticles)
+    public articles: Article
+
+
+
+}
