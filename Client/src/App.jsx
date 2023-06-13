f6b00e793620012356835d2f4cee6dabf3a12e32
Franco <francofran1998@gmail.com>
2023-06-12 21:55:44 -0300

    Add styles of Card component

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,14 +1,14 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import "./App.css";
-
 import Home from "./components/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
-    <>
+    <ChakraProvider>
       <NavBar />
       <Home />
-    </>
+    </ChakraProvider>
   );
 }
 
