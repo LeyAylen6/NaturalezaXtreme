b1b78d9ee2650bbf7837c04a077bcda33a9faa7a
Seba-Concheso <sebastianconcheso@gmail.com>
2023-06-13 23:53:20 -0300

    admin view to do CRUD

--- Client/src/components/Admin/Admin.jsx
+++ Client/src/components/Admin/Admin.jsx
@@ -1,56 +1,122 @@
-import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
-import { SimpleGrid } from "@chakra-ui/react";
+import { Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption, TableContainer, Box } from "@chakra-ui/react";
+
 import { Button, ButtonGroup } from "@chakra-ui/react";
+// import { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
 
 const Admin = () => {
+  // const dispatch = useDispatch();
+  // const products = useSelector((state) => state.products);
+
+  // useEffect(() => {
+  //   dispatch(getProducts());
+  // }, [dispatch]);
+
   return (
-    <Card>
-      <h1>Admin</h1>
-      <SimpleGrid columns={2} spacing={10}>
-        <CardBody>
-          <CardHeader>Products</CardHeader>
-          <Button colorScheme="blue">Add </Button>
-          <Button colorScheme="orange">Edit</Button>
-          <Button colorScheme="red">Delete</Button>
-        </CardBody>
-        <CardBody>
-          <CardHeader>Users</CardHeader>
-          <Button colorScheme="blue">Add User</Button>
-          <button>Edit User</button>
-          <button>Delete User</button>
-        </CardBody>
-        <CardBody>
-          <CardHeader>Orders</CardHeader>
-          <Button colorScheme="blue">Add Order</Button>
-          <button>Edit Order</button>
-          <button>Delete Order</button>
-        </CardBody>
-        <CardBody>
-          <CardHeader>Categories</CardHeader>
-          <Button colorScheme="blue">Add Category</Button>
-          <button>Edit Category</button>
-          <button>Delete Category</button>
-        </CardBody>
-        <CardBody>
-          <CardHeader>Reviews</CardHeader>
-          <Button colorScheme="blue">Add Review</Button>
-          <button>Edit Review</button>
-          <button>Delete Review</button>
-        </CardBody>
-        <CardBody>
-          <CardHeader>Admins</CardHeader>
-          <Button colorScheme="blue">Add Admin</Button>
-          <button>Edit Admin</button>
-          <button>Delete Admin</button>
-        </CardBody>
-        <CardBody>
-          <CardHeader>Images</CardHeader>
-          <Button colorScheme="blue">Add Image</Button>
-          <button>Edit Image</button>
-          <button>Delete Image</button>
-        </CardBody>
-      </SimpleGrid>
-    </Card>
+    <TableContainer
+      maxW="container.xl"
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      boxShadow="lg"
+      p="6"
+      m="6"
+      bg="white"
+      rounded="md"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Table>
+        <Thead>
+          <TableCaption fontSize={"24px"}>List of products in store</TableCaption>
+          <Tr bg="gray.100" border="1px" borderColor="gray.300" p="6" m="6">
+            <Th>Image</Th>
+            <Th>Product</Th>
+            <Th>description</Th>
+            <Th>price</Th>
+            <Th>stock</Th>
+            <Th display={"flex"} justifyContent={"center"}>
+              Actions
+            </Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>
+              <Box w="70px" h="45px" maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+                <img src="https://bit.ly/2Z4KKcF" alt="imagen" width="100%" height="100%" />
+              </Box>
+            </Td>
+            <Td>Camisa</Td>
+            <Td>Camisa de algodon</Td>
+            <Td>1000</Td>
+            <Td>10</Td>
+            <Td>
+              <ButtonGroup size="md" variant={"solid"}>
+                <Button colorScheme="yellow">View</Button>
+                <Button colorScheme="green">Edit</Button>
+                <Button colorScheme="red">Delete</Button>
+              </ButtonGroup>
+            </Td>
+          </Tr>
+          <Tr>
+            <Td>
+              <Box w="70px" h="45px" maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+                <img src="https://bit.ly/2Z4KKcF" alt="imagen" width="100%" height="100%" />
+              </Box>
+            </Td>
+            <Td>Zapatilla</Td>
+            <Td>Zapatilla trekking</Td>
+            <Td>5000</Td>
+            <Td>10</Td>
+            <Td>
+              <ButtonGroup size="sm">
+                <Button colorScheme="yellow">View</Button>
+                <Button colorScheme="green">Edit</Button>
+                <Button colorScheme="red">Delete</Button>
+              </ButtonGroup>
+            </Td>
+          </Tr>
+          <Tr>
+            <Td>
+              <Box w="70px" h="45px" maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+                <img src="https://bit.ly/2Z4KKcF" alt="imagen" width="100%" height="100%" />
+              </Box>
+            </Td>
+            <Td>Campera</Td>
+            <Td>Campera de nieve</Td>
+            <Td>10000</Td>
+            <Td>10</Td>
+            <Td>
+              <ButtonGroup size="lg" variant={"solid"}>
+                <Button colorScheme="yellow">View</Button>
+                <Button colorScheme="green">Edit</Button>
+                <Button colorScheme="red">Delete</Button>
+              </ButtonGroup>
+            </Td>
+          </Tr>
+        </Tbody>
+        {/* <Tbody>
+          {products.map((product) => (
+            <Tr key={product.id}>
+              <Td>{product.name}</Td>
+              <Td>{product.description}</Td>
+              <Td>{product.price}</Td>
+              <Td>{product.stock}</Td>
+            </Tr>
+          ))}
+        </Tbody> */}
+        <Tfoot>
+          <Tr>
+            <Th>Image</Th>
+            <Th>Product</Th>
+            <Th>Description</Th>
+            <Th>Price</Th>
+            <Th>Stock</Th>
+          </Tr>
+        </Tfoot>
+      </Table>
+    </TableContainer>
   );
 };
 
