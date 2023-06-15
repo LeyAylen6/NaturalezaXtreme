62b41aa8999a1b2eb91c9a4f0510e54e6097ba34
Franco <francofran1998@gmail.com>
2023-06-15 10:50:46 -0300

    Actualizacion de redux para ruta getArticles

--- Client/src/redux/actions/actions.js
+++ Client/src/redux/actions/actions.js
@@ -1,6 +1,7 @@
 export const ADD_FAV = "ADD_FAV";
 export const REMOVE_FAV = "REMOVE_FAV";
 export const GET_ALL_PRODUCTS = "GET_ALL_PRODUCTS";
+import axios from "axios";
 
 export const addFav = (character) => {
   return { type: ADD_FAV, payload: character };
@@ -8,6 +9,10 @@ export const addFav = (character) => {
 export const removeFav = (id) => {
   return { type: REMOVE_FAV, payload: id };
 };
-export const getAllProducts = (products) => {
-  return { type: GET_ALL_PRODUCTS, payload: products };
+export const getAllProducts = () => {
+  return async function (dispatch) {
+    const apiData = await axios.get("http://localhost:3001/articles");
+    const products = apiData.data;
+    dispatch({ type: GET_ALL_PRODUCTS, payload: products });
+  };
 };
