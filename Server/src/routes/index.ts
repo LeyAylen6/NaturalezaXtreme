1a04c50438f0a2e51705f2d2839ea8f2c6da1f79
Alejandro Agustin Diaz Garro <126215395+AAgustinDiazGarro@users.noreply.github.com>
2023-06-14 21:31:17 -0300

    Update index.ts
    Se habian borrado los import... Las volvi a colocar y funciona

--- Server/src/routes/index.ts
+++ Server/src/routes/index.ts
@@ -1,3 +1,12 @@
+import {Router} from 'express'
+import getArticles from './getArticles';
+import postArticle from './postArticle';
+import getArticlesByName from './getArticlesByName';
+import bulkArticles from './bulkArticles';
+import getArticlesById from './getArticlesById';
+import updateArticle from './updateArticle';
+const router = Router();
+
 router.post('/bulkArticles', bulkArticles)
 router.post('/articles', postArticle)
 router.get('/articles', getArticles)
