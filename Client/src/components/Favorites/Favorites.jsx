25408b020b3ddb026ba75f482a93599dccab11d5
Franco <francofran1998@gmail.com>
2023-06-15 22:56:18 -0300

    Correccion de error en favorite

--- Client/src/components/Favorites/Favorites.jsx
+++ Client/src/components/Favorites/Favorites.jsx
@@ -1,29 +1,9 @@
-<<<<<<< HEAD
 import { useEffect } from "react";
 import Tarjeta from "../Card/Card";
 import { connect, useDispatch, useSelector } from "react-redux";
-import { getAllProducts } from "../../redux/actions/actions";
 import { Grid } from "@chakra-ui/react";
-const Favorites = ({ myFavorites }) => {
-  const dispatch = useDispatch();
-  const productTest = useSelector((state) => state.allProducts);
-
-  useEffect(() => {
-    dispatch(getAllProducts());
-  }, []);
-=======
-import { useEffect } from "react"
-import Tarjeta from "../Card/Card"
-import {connect, useDispatch, useSelector } from 'react-redux'
-import { Grid } from "@chakra-ui/react"
 const Favorites = (props) => {
-    const dispatch = useDispatch()
-    
-    return (
-        <div>
-            <select>
-                <option value="allProducts">All Products</option>
->>>>>>> dev
+  const dispatch = useDispatch();
 
   return (
     <div>
@@ -36,52 +16,25 @@ const Favorites = (props) => {
 
         <option value="Pants">Pants</option>
 
-<<<<<<< HEAD
         <option value="Accesories">Accesories</option>
-=======
-                <option value='jacket'>Jacket</option>
-            </select>
-            
-            <div>
-            <Grid templateColumns="repeat(4, 1fr)" justifyItems={"center"}>
-        {
-            props.myFavorites.map((product)=> {
-                return (
-                    <Tarjeta
-                    button = {false}
-                    image = {product.image}
-                    name={product.name}
-                    description = {product.description}
-                    price = {product.price}
-                    rating ={product.rating}
-                    color= {product.color}
-                    gender= {product.gender}
-                   
-                    key={product.id}
-                    />
-                )
-            })
-        }
-       </Grid>
-        </div>
->>>>>>> dev
 
         <option value="jacket">Jacket</option>
       </select>
 
       <div>
         <Grid templateColumns="repeat(4, 1fr)" justifyItems={"center"}>
-          {productTest?.map((product) => {
+          {props.myFavorites.map((product) => {
             return (
               <Tarjeta
-                key={product.id}
-                name={product.name}
-                price={product.price}
+                button={false}
                 image={product.image}
+                name={product.name}
                 description={product.description}
+                price={product.price}
                 rating={product.rating}
                 color={product.color}
                 gender={product.gender}
+                key={product.id}
               />
             );
           })}
