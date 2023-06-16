7b0a7abd0c0dd7be49f3bff4d7e18f4c55f768ca
LeslieQuet <quetglasleslie@gmail.com>
2023-06-16 10:43:48 -0300

    Article Entity modified (articleID property) & filters integrated on getArticles route)

--- Server/src/controllers/articles/getArticlesByNameController.ts
+++ Server/src/controllers/articles/getArticlesByNameController.ts
@@ -2,13 +2,28 @@ import { Article } from "../../entities/articleEntity"
 import { AppDataSource } from "../../db"
 import { ILike } from "typeorm";
 
-const getArticlesByNameController = async(name: string) => {
+export interface FilterArticlesStructure {
+    gender?: string,
+    type?: string,
+    color?: string
+}
 
-    const articlesFound = await AppDataSource.getRepository(Article).find({ 
+const getArticlesByNameController = async(name: string, gender: string, type:string, color: string) => {
+
+    const filters: FilterArticlesStructure = {}
+    if(gender) filters.gender = gender;
+    if(type) filters.type = type;
+    if(color) filters.color = color;
+    console.log(color)
+
+    const query = {
         where: {
-            name: ILike(`%${name}%`)
+          name: ILike(`%${name}%`),
+          ...filters, // Agrega los filtros al objeto de búsqueda
         },
-    });
+      };
+
+    const articlesFound = await AppDataSource.getRepository(Article).find(query);
 
     if (articlesFound.length === 0) throw new Error(`no matches were found for your search`)
 
