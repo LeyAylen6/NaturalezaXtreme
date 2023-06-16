e2bedad6d2bedccafc09ea66bf23a9ed338f323f
Leila <leiisalguero@gmaill.com>
2023-06-16 11:15:51 -0300

    add validation in updateArticleController

--- Server/src/controllers/articles/updateArticleController.ts
+++ Server/src/controllers/articles/updateArticleController.ts
@@ -6,6 +6,7 @@ import getArticlesByIdController from "./getArticleByIdController";
 const updateArticleController = async(newArticle: articleStructure) => {
 
     const articleFound = await getArticlesByIdController(newArticle.id)
+    if(newArticle.active !== articleFound.active) throw new Error('Cannot modify the active property of the article')
 
     await AppDataSource.getRepository(Article).merge(articleFound, newArticle)
     const results = await AppDataSource.getRepository(Article).save(articleFound)
