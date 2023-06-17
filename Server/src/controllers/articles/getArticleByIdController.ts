9573581091fbcaf6641502101969b36d7a8377f8
LeslieQuet <quetglasleslie@gmail.com>
2023-06-17 11:18:27 -0300

    ByArticleId returns the article

--- Server/src/controllers/articles/getArticleByIdController.ts
+++ Server/src/controllers/articles/getArticleByIdController.ts
@@ -3,20 +3,27 @@ import { Article } from "../../entities/articleEntity"
 
 const getArticlesByIdController = async(id: number, articleID?: string) => {
 
+    // let articleData: {
+    //     id: number;
+    //     message: string;
+    // };    
+
     if(articleID !== undefined){
-        const articleFound = await AppDataSource.getRepository(Article).findOneBy({
+        const articleFound = await Article.findOneBy({
             articleID: articleID
         })        
         if (!articleFound) throw new Error("There's no item on existence with that article number")
-        return articleFound;
+        //En caso de enviar algo como lo de abajo hay que modificar los controladores de update y deactivate
+        // articleData = {id: articleFound.id, message: "The article is on existence"}
+        // return articleData;
+        return articleFound
     }
 
-    const articleFound = await AppDataSource.getRepository(Article).findOneBy({
+    const articleFound = await Article.findOneBy({
         id: id
     })
 
     if (!articleFound) throw new Error('There is no article with that id')
-    
     return articleFound;
 }
 
