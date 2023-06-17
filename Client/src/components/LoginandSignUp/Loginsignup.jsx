6149b78592797495002902feeb5c48a9fe508449
Valkiria5 <dharmavaninaramayoquilinan@gmail.com>
2023-06-17 15:01:31 -0300

    login and sign up styles

--- Client/src/components/LoginandSignUp/Loginsignup.jsx
+++ Client/src/components/LoginandSignUp/Loginsignup.jsx
@@ -1,13 +1,23 @@
 import Login from "./Login/Login";
 import Signup from "./Signup/Signup";
+import { Box, HStack, Stack } from "@chakra-ui/react";
 
 const Loginsingup = () => {
     return (
-        <div>
-            <Login/>
-            <Signup/>
-   
-        </div>
+        <Box 
+        backgroundImage= {"url(https://i.blogs.es/199e7b/simon-english-672450-unsplash/1366_2000.jpeg)"}
+        backgroundSize="cover"
+        backgroundPosition="center"
+        >
+        <HStack spacing='24px'>
+        <Box display="flex" justifyContent="center" alignItems="center" h="100vh" marginLeft="300px">
+            <Login/> 
+        </Box>
+        <Box>
+            <Signup/> 
+        </Box>
+        </HStack> 
+        </Box>
     )
 }
 export default Loginsingup;
--- Client/src/components/LoginandSignUp/Signup/Signup.jsx
+++ Client/src/components/LoginandSignUp/Signup/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { validationsignup } from "../../Validation/validationsignup"; 
-
+import { Box, Button, Card, Container, FormControl, FormLabel, HStack, Heading, Input, Select, Text } from "@chakra-ui/react";
 const Signup =()=> {
     const [signuserData, setsignUserData] = useState({
         name: '',
@@ -24,42 +24,50 @@ const Signup =()=> {
     };
  
     return (
-        <div>
-            <h1>I want to be part</h1>
+        <Container marginTop={10} >
+          <Card padding={4} background={`rgba(255, 255, 255, 0.3)`}>
+            <Heading fontSize="25px" marginBottom="25px" marginTop="20px">I WANT TO BE PART</Heading>
+            <Heading fontSize="X" marginBottom="20px"> complete the information</Heading>
             <form>
-            <label>Name</label>
-                <input 
+            <FormControl>
+              <FormLabel>Name</FormLabel>
+                <Input 
                 name="name"
                 type="text"
                 onChange={handleChange}
+                placeholder="Enter your name"
                 />
                   {errors.name && <p>{errors.name}</p>}
-                <label>LastName</label>
-                <input 
+              <FormLabel>Lastname</FormLabel>
+                <Input 
                 name="lastname"
                 type="text"
                 onChange={handleChange}
+                placeholder="Enter your LastName"
                 />
                   {errors.lastname && <p>{errors.lastname}</p>}
-                <label>Email</label>
-                <input 
+               <FormLabel>Email</FormLabel>
+                <Input 
                 name="email"
                 type="text"
                 onChange={handleChange}
+                placeholder="Enter your Email ej: Email@gmail.com"
                 />
                   {errors.email && <p>{errors.email}</p>}
-                <label>Password</label>
-                <input 
+               <FormLabel>Password</FormLabel>
+                <Input 
                 name="password"
                 type="password"
                 onChange={handleChange}
+                placeholder="Enter your new password"
                 />
                   {errors.password&& <p>{errors.password}</p>}
-                <button type="submit" disabled={errors.email || errors.password || errors.name || errors.lastname || !signuserData.email || !signuserData.password || !signuserData.name ||  !signuserData.lastname }>Sign Up</button>
-
+                <Button backgroundColor= "black" color="white" marginTop= "15px" type="submit" disabled={errors.email || errors.password || errors.name || errors.lastname || !signuserData.email || !signuserData.password || !signuserData.name ||  !signuserData.lastname }>Sign Up</Button>
+                </FormControl>
             </form>
+            </Card>
+        </Container>
         
-        </div>
     )
 }
 export default Signup; 
