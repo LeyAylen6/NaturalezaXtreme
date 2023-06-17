1109a448397c512c7e5629030a3b22bf6f64665b
LeslieQuet <quetglasleslie@gmail.com>
2023-06-17 09:17:51 -0300

    Folders/files/function names modified

--- Server/src/controllers/articles/deactivateArticleController.ts
+++ Server/src/controllers/articles/deactivateArticleController.ts
@@ -7,7 +7,7 @@ const desactivateArticleController = async(id: number, active: boolean) => {
     const articleFound = await getArticlesByIdController(id)
     let newArticle = {...articleFound};
 
-    newArticle.active = active
+    newArticle.active = active;
 
     await AppDataSource.getRepository(Article).merge(articleFound, newArticle)
     const results = await AppDataSource.getRepository(Article).save(articleFound)
