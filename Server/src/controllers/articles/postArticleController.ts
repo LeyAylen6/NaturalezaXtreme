706722050ffd0963ea8cf560f4f1a2fa7d2441a3
LeslieQuet <quetglasleslie@gmail.com>
2023-06-17 14:49:30 -0300

    Bulkcreate Articles don't longer duplicate

--- Server/src/controllers/articles/postArticleController.ts
+++ Server/src/controllers/articles/postArticleController.ts
@@ -1,12 +1,10 @@
 import { Article } from "../../entities/articleEntity"
-import { AppDataSource } from "../../db";
 import { articleStructure } from "../../interfaces/articleStructure";
 
 const postArticleController = async(article: articleStructure) => {
     const articleID: string = article.articleID;
 
-    const articleFound = await AppDataSource.getRepository(Article).findOneBy({articleID: articleID})
-    console.log(articleID)
+    const articleFound = await Article.findOneBy({articleID: articleID})
 
     if (articleFound) throw new Error('The article already exist') 
 
