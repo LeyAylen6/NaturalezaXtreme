09b79e36ed0a0421307a9e04540585da54abe980
unknown <agustin87_7@hotmail.com>
2023-06-20 12:23:46 -0300

    traeAllUser

--- Client/src/redux/actions/actionsUsers.js
+++ Client/src/redux/actions/actionsUsers.js
@@ -2,9 +2,9 @@ export const GET_USERS = "GET_USERS";
 export const POST_USERS = "POST_USERS";
 import axios from "axios";
 
-export const getUsers = () => {
+export const getUsers = (id) => {
   return async function (dispatch) {
-    const apiData = await axios.get(`http://localhost:3001/users?id=${id}`);
+    const apiData = await axios.get(`http://localhost:3001/user?id=${id}`);
     const users = apiData.data;
     dispatch({ type: GET_USERS, payload: users });
   };
