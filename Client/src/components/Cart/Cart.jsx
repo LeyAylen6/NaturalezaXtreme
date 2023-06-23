0806833367e628092500b063590b7d2ccae2a919
Seba-Concheso <sebastianconcheso@gmail.com>
2023-06-23 12:45:21 -0300

    solucion de la imagen del carrito

--- Client/src/components/Cart/Cart.jsx
+++ Client/src/components/Cart/Cart.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import { Box, Heading, UnorderedList, ListItem, Button } from "@chakra-ui/react";
-import { useSelector, useDispatch } from 'react-redux';
-import { removeFromCart, increaseQuantity, decreaseQuantity } from '../../redux/actions/actions';
+import { Box, Heading, UnorderedList, ListItem, Button, Image } from "@chakra-ui/react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeFromCart, increaseQuantity, decreaseQuantity } from "../../redux/actions/actions";
 
 const Cart = () => {
   const cartArticles = useSelector((state) => state.cartArticles);
@@ -35,67 +34,51 @@ const Cart = () => {
   const handleBuy = () => {
     // Aquí puedes realizar acciones adicionales al finalizar la compra
     // Por ejemplo, vaciar el carrito, enviar una solicitud de compra, etc.
-    alert('¡Compra realizada con éxito!');
+    alert("¡Compra realizada con éxito!");
   };
 
   return (
-    <Box border={"2px"} >
-      <Heading>
-        Shopping Cart
-      </Heading>
+    <Box border={"2px"}>
+      <Heading>Shopping Cart</Heading>
       <UnorderedList>
         {cartArticles.length === 0 ? (
           <Heading fontSize="14px">No tienes productos en el carrito</Heading>
         ) : (
-          cartArticles.map((item) => (
+          cartArticles?.map((item) => (
             <ListItem key={item.id} mb={2}>
-              <Box border={"1px"} display={"flex"} justifyContent={"space-between"} >
-              <Box>
-                <image src={item.image} alt={item.name} width="100px" />
-              </Box>
-              <Box display="flex" justifyContent="center" alignItems="center">
-      
-              {item.name} - Cantidad: {item.quantity}
-              </Box>
-              
-              <Box display="flex" justifyContent="center" alignItems="center">
-                <p> U$S: {item.price} </p>
-              </Box>
-              <Box display="flex" justifyContent="center" alignItems="center">
-              <Button
-                colorScheme="blue"
-                size="sm"
-                ml={2}
-                onClick={() => handleIncreaseQuantity(item.id)}
-              >
-                +
-              </Button>
-              <Button
-                colorScheme="red"
-                size="sm"
-                ml={2}
-                onClick={() => handleDecreaseQuantity(item.id)}
-              >
-                -
-              </Button>
-              <Button
-                colorScheme="red"
-                size="sm"
-                ml={2}
-                onClick={() => handleRemoveFromCart(item.id)}
-              >
-                Eliminar
-              </Button>
-              </Box>
+              <Box border={"1px"} display={"flex"} justifyContent={"space-between"}>
+                <Box>
+                  <Image src={item.image} alt={item.name} width="100px" />
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  {item.name} - Cantidad: {item.quantity}
+                </Box>
+
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <p> U$S: {item.price} </p>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button colorScheme="blue" size="sm" ml={2} onClick={() => handleIncreaseQuantity(item.id)}>
+                    +
+                  </Button>
+                  <Button colorScheme="red" size="sm" ml={2} onClick={() => handleDecreaseQuantity(item.id)}>
+                    -
+                  </Button>
+                  <Button colorScheme="red" size="sm" ml={2} onClick={() => handleRemoveFromCart(item.id)}>
+                    Eliminar
+                  </Button>
+                </Box>
               </Box>
             </ListItem>
           ))
         )}
-      
-      <Box mt={4} color="white" bg="#2C2C2C" display={"flex"} justifyContent={"center"}>
-        Total: ${calculateTotal()}
-      </Box>
-      <Button mt={4} onClick={handleBuy}>Comprar</Button>
+
+        <Box mt={4} color="white" bg="#2C2C2C" display={"flex"} justifyContent={"center"}>
+          Total: ${calculateTotal()}
+        </Box>
+        <Button mt={4} onClick={handleBuy}>
+          Comprar
+        </Button>
       </UnorderedList>
     </Box>
   );
