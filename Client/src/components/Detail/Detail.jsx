f7f13c99a57f131850d5a5fe2611a898f262b9a9
nicoflorentin <nicoflorentn@gmail.com>
2023-06-24 17:56:04 -0300

    refactor

--- Client/src/components/Detail/Detail.jsx
+++ Client/src/components/Detail/Detail.jsx
@@ -1,19 +1,12 @@
-import React, { useEffect, useState } from "react";
-import Comments from "../Comments/Comments";
-import { addFav, removeFav, resState } from "../../redux/actions/actions";
-import { useSelector, useDispatch } from "react-redux";
-import { getDetail, createPayment } from "../../redux/actions/actions";
-import { useNavigate, useParams } from "react-router-dom";
-import {
-	Box,
-	Image,
-	Flex,
-	Button,
-	Text,
-	Select,
-	Stack,
-} from "@chakra-ui/react";
-import { addToCart, removeFromCart } from "../../redux/actions/cartActions";
+import React, { useEffect, useState } from "react"
+import Comments from "../Comments/Comments"
+import { addFav, removeFav, resState } from "../../redux/actions/actions"
+import { useSelector, useDispatch } from "react-redux"
+import { getDetail, createPayment } from "../../redux/actions/actions"
+import { useNavigate, useParams } from "react-router-dom"
+import { Box, Image, Flex, Button, Text, Select, Stack } from "@chakra-ui/react"
+import { addToCart, removeFromCart } from "../../redux/actions/cartActions"
+import sizeMapper from "./utils/sizeMapper"
 
 const initProductSelections = {
 	id: "",
@@ -25,48 +18,70 @@ const initProductSelections = {
 	price: 0,
 	size: "",
 	shoeSize: "",
-};
+}
 
 const Detail = () => {
-	const { id } = useParams();
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
+	const { id } = useParams()
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
 
-	let myFavorites = useSelector((state) => state.myFavorites);
-	const articleDetail = useSelector((state) => state.detail);
-	const cartArticles = useSelector((state) => state.cartArticles);
-	const paymentLink = useSelector((state) => state.paymentLink);
+	let myFavorites = useSelector(state => state.myFavorites)
+	const articleDetail = useSelector(state => state.detail)
+	const cartArticles = useSelector(state => state.cartArticles)
+	const paymentLink = useSelector(state => state.paymentLink)
 
-	const [isFavorite, setIsFavorite] = useState(false);
-	const [isInCart, setIsInCart] = useState(false);
-	const [productSelections, setProductSelections] = useState(
-		initProductSelections
-	);
+	const [isFavorite, setIsFavorite] = useState(false)
+	const [isInCart, setIsInCart] = useState(false)
+	const [inStock, setInStock] = useState(false)
+	const [productSelections, setProductSelections] = useState(initProductSelections)
 
-	console.log("article detail", articleDetail);
+	console.log("article detail", articleDetail)
 	// console.log("cart articles", cartArticles);
 	// console.log("product selections", productSelections);
+	console.log(inStock)
 
 	useEffect(() => {
-		dispatch(getDetail(id));
-		myFavorites.forEach((fav) => {
+		dispatch(getDetail(id))
+		myFavorites.forEach(fav => {
 			if (fav.id == id) {
-				setIsFavorite(true);
+				setIsFavorite(true)
 			}
-		});
-		cartArticles.forEach((article) => {
+		})
+		cartArticles.forEach(article => {
 			if (article.id == id) {
-				setIsInCart(true);
+				setIsInCart(true)
 			}
-		});
+		})
 		return () => {
-			dispatch(resState());
-		};
-	}, []);
+			dispatch(resState())
+		}
+	}, [])
+
+	useEffect(() => {
+		if (articleDetail.type === "shoes") {
+			for (const size in articleDetail.shoeSize) {
+				if (articleDetail.shoeSize.hasOwnProperty(size)) {
+					if (articleDetail.shoeSize[size] !== 0) {
+						setInStock(true)
+						break
+					}
+				}
+			}
+		} else {
+			for (const size in articleDetail.size) {
+				if (articleDetail.size.hasOwnProperty(size)) {
+					if (articleDetail.size[size] !== 0) {
+						setInStock(true)
+						break
+					}
+				}
+			}
+		}
+	}, [articleDetail])
 
 	//handler de los campos
-	const handleChange = (event) => {
-		const { name: property, value } = event.target;
+	const handleChange = event => {
+		const { name: property, value } = event.target
 		// console.log('handling change')
 		// console.log(property, value);
 		setProductSelections({
@@ -78,125 +93,62 @@ const Detail = () => {
 			quantity: 1,
 			price: articleDetail.price,
 			[property]: value,
-		});
-	};
-
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		dispatch(addToCart(productSelections));
-		navigate("/cart");
-	};
-
-	//boton agregar al carrito
-	const handleAddToCart = (event) => {
-		event.preventDefault();
-		if (isInCart) {
-			setIsInCart(false);
-			dispatch(removeFromCart(id));
-		} else {
-			setIsInCart(true);
-			dispatch(addToCart(productSelections));
-		}
-	};
-
-	//boton agregar a favoritos
-	const handleFavorites = (event) => {
-		event.preventDefault();
-		console.log(isFavorite);
-		if (isFavorite) {
-			setIsFavorite(false);
-			dispatch(removeFav(id));
-		} else {
-			setIsFavorite(true);
-			dispatch(addFav(articleDetail));
-		}
-	};
-
-	const stockHandler = () => {
-		let stock = false;
-
-		if (articleDetail.type === "shoe") {
-			for (const size in articleDetail.shoeSize) {
-				if (articleDetail.shoeSize.hasOwnProperty(size)) {
-					if (articleDetail.shoeSize[size] !== 0) {
-						stock = true;
-						break;
-					}
-				}
-			}
-		} else {
-			for (const size in articleDetail.size) {
-				if (articleDetail.size.hasOwnProperty(size)) {
-					if (articleDetail.size[size] !== 0) {
-						stock = true;
-						break;
-					}
-				}
-			}
-		}
-
-		if (stock) {
-			return (
-				<Box bg="#48BB78" p="5px" textAlign="center">
-					Product in stock
-				</Box>
-			);
-		} else {
-			return (
-				<Box bg="tomato" p="5px" textAlign="center">
-					Product out of stock
-				</Box>
-			);
-		}
-	};
-
-	//mapea a string los objetos de talle de ropa
-	let clotheSizeOptions = [];
-	if (articleDetail.size && typeof articleDetail.size === "object") {
-		clotheSizeOptions = Object.entries(articleDetail.size).map(
-			([key, value]) => (
-				<option key={key} value={key} disabled={!!!value}>
-					"{key}"
-				</option>
-			)
-		);
+		})
 	}
 
-	//mapea a string los objetos de talle de zapatillas
-	let shoeSizeOptions = [];
-	if (articleDetail.shoeSize && typeof articleDetail.shoeSize === "object") {
-		shoeSizeOptions = Object.entries(articleDetail.shoeSize).map(
-			([key, value]) => (
-				<option key={key} value={key} disabled={!!!value}>
-					N°{key}
-				</option>
-			)
-		);
+	const handleSubmit = event => {
+		event.preventDefault()
+		dispatch(addToCart(productSelections))
+		navigate("/cart")
+	}
+
+	//boton agregar al carrito
+	const handleAddToCart = event => {
+		event.preventDefault()
+		if (isInCart) {
+			setIsInCart(false)
+			dispatch(removeFromCart(id))
+		} else {
+			setIsInCart(true)
+			dispatch(addToCart(productSelections))
+		}
+	}
+
+	//boton agregar a favoritos
+	const handleFavorites = event => {
+		event.preventDefault()
+		console.log(isFavorite)
+		if (isFavorite) {
+			setIsFavorite(false)
+			dispatch(removeFav(id))
+		} else {
+			setIsFavorite(true)
+			dispatch(addFav(articleDetail))
+		}
+	}
+
+	const stockDisplayConfig = [
+		{ bg: "#48BB78", content: "Product in stock", hidden: !inStock ? "hidden" : null },
+		{ bg: "tomato", content: "Product out of stock", hidden: inStock ? "hidden" : null },
+	]
+	const StockDisplay = ({ content, ...config }) => {
+		console.log("display", inStock)
+		return <Box {...config}>{content}</Box>
 	}
 
 	const enableButtonHandler = () => {
-		if (
-			productSelections.size === "" ||
-			productSelections.shoeSize === ""
-		) {
-			return true;
+		if (productSelections.size === "" || productSelections.shoeSize === "") {
+			return true
 		} else {
-			return false;
+			return false
 		}
-	};
-	// console.log("disable?", enableButtonHandler());
+	}
 
 	return (
 		<Flex align={"center"} mt="50px" direction="column">
 			<Flex id="2box container" flexDirection="row" gap="20px">
 				<Box>
-					<Image
-						mt="20px"
-						boxSize={"300px"}
-						objectFit={"cover"}
-						src={articleDetail.image}
-						alt={articleDetail.name}
-					/>
+					<Image mt="20px" boxSize={"300px"} objectFit={"cover"} src={articleDetail.image} alt={articleDetail.name} />
 				</Box>
 				<Box textAlign="left">
 					<Box fontSize="40px" fontWeight="semibold" width="100%">
@@ -218,15 +170,18 @@ const Detail = () => {
 							{articleDetail.type !== "shoes" ? (
 								<Select name="size" variant="filled" mt="20px">
 									<option value="">Choose size</option>
-									{clotheSizeOptions}
+									{sizeMapper(articleDetail)}
 								</Select>
 							) : (
 								<Select name="shoeSize" variant="filled">
 									<option value="">Choose size</option>
-									{shoeSizeOptions}
+									{sizeMapper(articleDetail)}
 								</Select>
 							)}
-							{stockHandler()}
+							{stockDisplayConfig.map(config => (
+								<StockDisplay {...config} />
+							))}
+
 							{/* <label>Quantity</label>
 						<input
 							type="number"
@@ -235,46 +190,23 @@ const Detail = () => {
 							min="1"
 							max={articleDetail.stock}
 						></input> */}
-							<Flex
-								id="buttons"
-								direction="row"
-								alignItems="stretch"
-								gap="10px"
-							>
+							<Flex id="buttons" direction="row" alignItems="stretch" gap="10px">
 								{!isFavorite ? (
-									<Button
-										onClick={handleFavorites}
-										flex="1"
-										m="10px"
-									>
+									<Button onClick={handleFavorites} flex="1" m="10px">
 										Add to favorites
 									</Button>
 								) : (
-									<Button
-										onClick={handleFavorites}
-										flex="1"
-										m="10px"
-									>
+									<Button onClick={handleFavorites} flex="1" m="10px">
 										Remove from favorites
 									</Button>
 								)}
 
 								{!isInCart ? (
-									<Button
-										onClick={handleAddToCart}
-										isDisabled={enableButtonHandler()}
-										flex="1"
-										m="10px"
-									>
+									<Button onClick={handleAddToCart} isDisabled={enableButtonHandler()} flex="1" m="10px">
 										Add to cart
 									</Button>
 								) : (
-									<Button
-										onClick={handleAddToCart}
-										isDisabled={enableButtonHandler()}
-										flex="1"
-										m="10px"
-									>
+									<Button onClick={handleAddToCart} isDisabled={enableButtonHandler()} flex="1" m="10px">
 										Remove from Cart
 									</Button>
 								)}
@@ -296,7 +228,7 @@ const Detail = () => {
 				<Comments comments={articleDetail.comments} />
 			</Flex>
 		</Flex>
-	);
-};
+	)
+}
 
-export default Detail;
+export default Detail
---
+++ Client/src/components/Detail/utils/sizeMapper.jsx
@@ -0,0 +1,27 @@
+const sizeMapper = detailObject => {
+	//mapea a string los objetos de talle de ropa
+	let clotheSizeOptions = []
+	if (detailObject.size && typeof detailObject.size === "object") {
+		clotheSizeOptions = Object.entries(detailObject.size).map(([key, value]) => (
+			<option key={key} value={key} disabled={!!!value}>
+				"{key}"
+			</option>
+		))
+	}
+
+	//mapea a string los objetos de talle de zapatillas
+	let shoeSizeOptions = []
+	if (detailObject.shoeSize && typeof detailObject.shoeSize === "object") {
+		shoeSizeOptions = Object.entries(detailObject.shoeSize).map(([key, value]) => (
+			<option key={key} value={key} disabled={!value}>
+				N°{key}
+			</option>
+		))
+	}
+
+	console.log(shoeSizeOptions, clotheSizeOptions)
+
+	return detailObject.type === "shoes" ? shoeSizeOptions : clotheSizeOptions
+}
+
+export default sizeMapper
