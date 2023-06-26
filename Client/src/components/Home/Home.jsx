c72504b557980799ee5e7ee97658e7ba56cb0a69
LeslieQuet <quetglasleslie@gmail.com>
2023-06-26 15:07:06 -0300

    Nodemon imports fix

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -4,12 +4,16 @@ import CardContainer from "../CardContainer/CardContainer";
 import Filters from "../Filters/Filters";
 import { Box } from "@chakra-ui/react";
 import { getArticles } from "../../redux/actions/actions";
+import { useAuth0 } from "@auth0/auth0-react"; 
 const Home = () => {
+  let {user} = useAuth0();
+  console.log(user)
 
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getArticles());
   }, []);
+
   return (
     <Box>
       <Filters />
