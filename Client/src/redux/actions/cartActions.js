87a02c6035a8102bb45dd1d720f745e9b6af9452
Seba-Concheso <sebastianconcheso@gmail.com>
2023-06-27 15:25:29 -0300

    probando cargar carrito

--- Client/src/redux/actions/cartActions.js
+++ Client/src/redux/actions/cartActions.js
@@ -1,41 +1,60 @@
 export const ADD_TO_CART = "ADD_TO_CART";
 export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
 export const CLEAR_CART = "CLEAR_CART";
-export const GET_CART = "GET_CART"; 
+export const GET_CART = "GET_CART";
+export const GET_PENDING_CART = "GET_PENDING_CART";
 
+import axios from "axios";
+const URL = "http://localhost:3001/";
 
-export const addToCart = (article) => {
-	return { type: ADD_TO_CART, payload: article }
+export const addToCart = (prod) => {
+  const { userId, id } = prod;
+  //   console.log(userId, id);
+  return async function (dispatch) {
+    const apiData = await axios.put(`${URL}shoppingcart?method=add`, { userId, articleId: id });
+    const product = apiData.data;
+    dispatch({ type: ADD_TO_CART, payload: product });
+  };
 };
 
 export const removeFromCart = (id) => {
-	return { type: REMOVE_FROM_CART, payload: id }
+  return { type: REMOVE_FROM_CART, payload: id };
 };
 
 export const clearCart = (id) => {
-	return { type: CLEAR_CART, payload: id }
+  return { type: CLEAR_CART, payload: id };
 };
 
 export const getCartById = (id) => async (dispatch) => {
-	try {
-	  const response = await axios.get(`${URL}/shoppingcart/${id}`);
-	  const cartById = response.data;
-  
-	  dispatch({
-		type: GET_CART,
-		payload: cartById,
-	  });
-} catch (error) {
-  if (error.response && error.response.data === 'There are no products in the cart') {
-	dispatch({
-	  type: GET_CART,
-	  payload: error.response.data,
-	});
-  } else {
-	dispatch({
-	  type: GET_CART,
-	  payload: 'An error occurred while fetching the cart.',
-	});
+  // console.log("id   action  " + idCart);
+  try {
+    const response = await axios.get(`${URL}/shoppingcart/${id}`);
+    const cartById = response.data;
+    console.log("cartById   action  " + cartById);
+
+    dispatch({
+      type: GET_CART,
+      payload: cartById,
+    });
+  } catch (error) {
+    if (error.response && error.response.data === "There are no products in the cart") {
+      dispatch({
+        type: GET_CART,
+        payload: error.response.data,
+      });
+    } else {
+      dispatch({
+        type: GET_CART,
+        payload: "An error occurred while fetching the cart.",
+      });
+    }
   }
-}
+};
+
+export const getPendingCart = (id) => {
+  return async function (dispatch) {
+    const apiData = await axios.get(`${URL}shoppingcart?userId=${id}&status=pending`);
+    const pendingCart = apiData.data;
+    dispatch({ type: GET_PENDING_CART, payload: pendingCart });
+  };
 };
--- Client/src/redux/reducer/reducer.js
+++ Client/src/redux/reducer/reducer.js
@@ -1,212 +1,207 @@
 import {
-	UPDATE_PRODUCT,
-	ADD_FAV,
-	REMOVE_FAV,
-	GET_ALL_PRODUCTS,
-	GET_ARTICLES_BY_QUERY,
-	FILTER_SEARCHBAR,
-	GET_DETAIL,
-	RES_STATE,
-	GET_PRODUCT_DESACTIVATE,
-	SET_PAYMENT_LINK,
-	ADD_PRODUCT,
-	GET_ARTICLE_ID,
-	GET_ARTICLES,
-	NEXT_PAGE,
-	PREV_PAGE,
-	SIGN_UP,
-	INCREASE_QUANTITY,
-	DECREASE_QUANTITY, 
+  UPDATE_PRODUCT,
+  ADD_FAV,
+  REMOVE_FAV,
+  GET_ALL_PRODUCTS,
+  GET_ARTICLES_BY_QUERY,
+  FILTER_SEARCHBAR,
+  GET_DETAIL,
+  RES_STATE,
+  GET_PRODUCT_DESACTIVATE,
+  SET_PAYMENT_LINK,
+  ADD_PRODUCT,
+  GET_ARTICLE_ID,
+  GET_ARTICLES,
+  NEXT_PAGE,
+  PREV_PAGE,
+  SIGN_UP,
+  INCREASE_QUANTITY,
+  DECREASE_QUANTITY,
 } from "../actions/actions";
-import { GET_USERS, POST_USERS, USER_ID} from "../actions/actionsUsers";
+import { GET_USERS, POST_USERS, USER_ID } from "../actions/actionsUsers";
 import { FILTER_COMBINATED } from "../actions/actionFilters";
-import {
-	ADD_TO_CART,
-	REMOVE_FROM_CART,
-	CLEAR_CART,
-	GET_CART,
-} from "../actions/cartActions";
+import { ADD_TO_CART, REMOVE_FROM_CART, CLEAR_CART, GET_CART, GET_PENDING_CART } from "../actions/cartActions";
 
 const initialState = {
-	users: [],
-	myFavorites: [],
-	allProducts: [],
-	detail: {},
-	articleById: {},
-	paymentLink: null,
-	signUp: true,
-	articles: [],
-	cartArticles: [],
-	cart: [],
-	userId: null,
+  users: [],
+  myFavorites: [],
+  allProducts: [],
+  detail: {},
+  articleById: {},
+  paymentLink: null,
+  signUp: true,
+  articles: [],
+  cartArticles: [],
+  cart: [],
+  userId: 0,
+  pendingCart: [],
 };
 
 const reducer = (state = initialState, action) => {
-	switch (action.type) {
-		case USER_ID:
-			return {
-				userId: action.payload,
-			}
-		case FILTER_COMBINATED:
-			return {
-				...state,
-				articles: action.payload,
-			};
-		case FILTER_SEARCHBAR:
-			return {
-				...state,
-				articles: action.payload,
-			};
-		case GET_ARTICLES_BY_QUERY:
-			return {
-				...state,
-				allProducts: action.payload,
-			};
-		case GET_ALL_PRODUCTS:
-			return {
-				...state,
-				allProducts: action.payload,
-			};
-		case ADD_FAV:
-			return {
-				...state,
-				myFavorites: [...state.myFavorites, action.payload],
-			};
-		case GET_USERS:
-			return {
-				...state,
-				users: action.payload,
-			};
-		case UPDATE_PRODUCT:
-			return {
-				...state,
-				articleById: action.payload,
-			};
+  switch (action.type) {
+    case USER_ID:
+      return {
+        ...state,
+        userId: action.payload,
+      };
+    case FILTER_COMBINATED:
+      return {
+        ...state,
+        articles: action.payload,
+      };
+    case FILTER_SEARCHBAR:
+      return {
+        ...state,
+        articles: action.payload,
+      };
+    case GET_ARTICLES_BY_QUERY:
+      return {
+        ...state,
+        allProducts: action.payload,
+      };
+    case GET_ALL_PRODUCTS:
+      return {
+        ...state,
+        allProducts: action.payload,
+      };
+    case ADD_FAV:
+      return {
+        ...state,
+        myFavorites: [...state.myFavorites, action.payload],
+      };
+    case GET_USERS:
+      return {
+        ...state,
+        users: action.payload,
+      };
+    case UPDATE_PRODUCT:
+      return {
+        ...state,
+        articleById: action.payload,
+      };
 
-		case REMOVE_FAV:
-			return {
-				...state,
-				myFavorites: state.myFavorites.filter(
-					(product) => product.id != action.payload
-				),
-			};
-		case GET_PRODUCT_DESACTIVATE:
-			return {
-				...state,
-			};
-		case GET_ARTICLES:
-			return {
-				...state,
-				articles: action.payload,
-			};
-		case NEXT_PAGE:
-			return {
-				...state,
-				articles: action.payload,
-			};
-		case PREV_PAGE:
-			return {
-				...state,
-				articles: action.payload,
-			};
+    case REMOVE_FAV:
+      return {
+        ...state,
+        myFavorites: state.myFavorites.filter((product) => product.id != action.payload),
+      };
+    case GET_PRODUCT_DESACTIVATE:
+      return {
+        ...state,
+      };
+    case GET_ARTICLES:
+      return {
+        ...state,
+        articles: action.payload,
+      };
+    case NEXT_PAGE:
+      return {
+        ...state,
+        articles: action.payload,
+      };
+    case PREV_PAGE:
+      return {
+        ...state,
+        articles: action.payload,
+      };
 
-		case REMOVE_FAV:
-			return {
-				...state,
-				myFavorites: state.myFavorites.filter(
-					(product) => product.id != action.payload
-				),
-			};
-		case GET_ARTICLE_ID:
-			return {
-				articleById: action.payload,
-			};
-		case GET_DETAIL:
-			return {
-				...state,
-				detail: action.payload,
-			};
-		case RES_STATE:
-			return {
-				...state,
-				detail: [],
-			};
+    case REMOVE_FAV:
+      return {
+        ...state,
+        myFavorites: state.myFavorites.filter((product) => product.id != action.payload),
+      };
+    case GET_ARTICLE_ID:
+      return {
+        articleById: action.payload,
+      };
+    case GET_DETAIL:
+      return {
+        ...state,
+        detail: action.payload,
+      };
+    case RES_STATE:
+      return {
+        ...state,
+        detail: [],
+      };
 
-		case SET_PAYMENT_LINK:
-			return {
-				...state,
-				paymentLink: action.payload,
-			};
+    case SET_PAYMENT_LINK:
+      return {
+        ...state,
+        paymentLink: action.payload,
+      };
 
-		case ADD_PRODUCT:
-			return {
-				...state,
-			};
-		case POST_USERS:
-			return {
-				...state,
-				users: action.payload,
-			};
-		case SIGN_UP:
-			return {
-				...state,
-				signUp: action.payload,
-			};
-		case ADD_TO_CART:
-			return {
-				...state,
-				cartArticles: [...state.cartArticles, action.payload],
-			};
-		case REMOVE_FROM_CART:
-			return {
-				...state,
-				cartArticles: [
-					...state.cartArticles.filter(
-						(article) => article.id != action.payload
-					),
-				],
-			};
-		case CLEAR_CART:
-			return {
-				...state,
-				cartArticles: [],
-			};
+    case ADD_PRODUCT:
+      return {
+        ...state,
+        allProducts: [...state.allProducts, action.payload],
+      };
+    case POST_USERS:
+      return {
+        ...state,
+        users: action.payload,
+      };
+    case SIGN_UP:
+      return {
+        ...state,
+        signUp: action.payload,
+      };
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartArticles: [...state.cartArticles, action.payload],
+      };
+    case REMOVE_FROM_CART:
+      return {
+        ...state,
+        cartArticles: [...state.cartArticles.filter((article) => article.id != action.payload)],
+      };
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartArticles: [],
+      };
 
-			case INCREASE_QUANTITY:
-				return {
-				  ...state,
-				  cartArticles: state.cartArticles.map((item) => {
-					if (item.id === action.payload) {
-					  return {
-						...item,
-						quantity: item.quantity + 1
-					  };
-					}
-					return item;
-				  })
-				};
-			  case DECREASE_QUANTITY:
-				return {
-				  ...state,
-				  cartArticles: state.cartArticles.map((item) => {
-					if (item.id === action.payload && item.quantity > 1) {
-					  return {
-						...item,
-						quantity: item.quantity - 1
-					  };
-					}
-					return item;
-				  })
-				};
-				case GET_CART: 
-				return {
-					...state,
-					cart: action.payload
-				}
+    case INCREASE_QUANTITY:
+      return {
+        ...state,
+        cartArticles: state.cartArticles.map((item) => {
+          if (item.id === action.payload) {
+            return {
+              ...item,
+              quantity: item.quantity + 1,
+            };
+          }
+          return item;
+        }),
+      };
+    case DECREASE_QUANTITY:
+      return {
+        ...state,
+        cartArticles: state.cartArticles.map((item) => {
+          if (item.id === action.payload && item.quantity > 1) {
+            return {
+              ...item,
+              quantity: item.quantity - 1,
+            };
+          }
+          return item;
+        }),
+      };
+    case GET_CART:
+      return {
+        ...state,
+        cart: action.payload,
+      };
+    case GET_PENDING_CART:
+      return {
+        ...state,
+        pendingCart: action.payload,
+      };
 
-		default:
-			return { ...state };
-	}
+    default:
+      return { ...state };
+  }
 };
 
 export default reducer;
