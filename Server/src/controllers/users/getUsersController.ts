6a340b84d237a834ab7111f266a6dafefd086cef
Leila <leiisalguero@gmaill.com>
2023-06-27 10:21:43 -0300

    fix in getAllUsers and getById in Carts

--- Server/src/controllers/users/getUsersController.ts
+++ Server/src/controllers/users/getUsersController.ts
@@ -7,15 +7,13 @@ const getUsersController = async(rol: string, active: boolean) => {
     let filters: filtersUserStructure = {}
     
     if (rol) filters.rol = rol;
-    if (active !== undefined) filters.active = active;
+    if (active == undefined) filters.active = active;
 
     const usersFound = await AppDataSource.getRepository(User).find({ where: filters });
     
     if(!usersFound.length) throw new Error ("No users available with that search")    
-    return usersFound;
     
-    // Agregar a la tabla un includes con las compras que hizo ese mismo usuario con su cuenta.
-    // Paginado
+    return usersFound;
 }
 
 export default getUsersController;
