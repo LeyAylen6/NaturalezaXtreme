464b33613ba15e21bd57b8b0cde1068e7d3b3e03
LeslieQuet <quetglasleslie@gmail.com>
2023-06-29 10:21:31 -0300

    About minor changes

--- Client/src/components/AboutUs/AboutUs.jsx
+++ Client/src/components/AboutUs/AboutUs.jsx
@@ -6,19 +6,19 @@ import image from "../../assets/about_bg.jpg";
 // bgColor="rgba(39, 57, 43)"
 const AboutUs = () => {
   return (
-      <Center bgImage={image} backgroundPosition="center" backgroundRepeat="no-repeat" bgSize="cover">
+      <Center bgImage={image} backgroundPosition="center" backgroundRepeat="no-repeat" bgSize="cover" >
         <Box/>
-      <Flex direction={"column"} width="85%" margin={7} gap='1' m="4">
-      <Box flex='1' p="5" borderWidth="5px" borderRadius="xl" bgColor="rgba(46, 48, 58, 0.8)">
+      <Flex direction={"column"} width="85%" margin={7} gap='1.5' m="3.5">
+      <Box flex='1' p="5" borderWidth="4px" borderRadius="xl" bgColor="rgba(46, 48, 58, 0.8)">
         <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short" color={"white"}>Welcome!</Text>
         <Text mt={2} fontSize="md" color={"white"}>Welcome to our online store, this could be your go-to destination for everything you need on any type of outdoor adventure.<br />We are proud to offer you a wide range of specialized articles and clothing for exciting activities such as trekking, climbing, and much more.</Text>
       </Box>
       <Spacer />
-      <Box flex='1' p="5" borderWidth="5px" borderRadius="xl" bgColor="rgba(46, 48, 58, 0.8)">
+      <Box flex='1' p="5" borderWidth="4px" borderRadius="xl" bgColor="rgba(46, 48, 58, 0.8)">
         <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short" color={"white"}>About us?</Text>
         <Text mt={2} fontSize="md" color={"white"}>We are students at Henry and we developed this project as part of the final stage of our training as full stack developers. We are:</Text>
         <Text mt={2} fontSize="md" color={"white"}><b>
-          <Link href='https://github.com/francolion' isExternal _hover={{color:"rgb(0, 255, 128)"}}>Franco Lionel Rodriguez Caserta</Link> * 
+          <Link href='https://www.linkedin.com/in/franco-rodriguez-375423264?trk=contact-info' isExternal _hover={{color:"rgb(0, 255, 128)"}}>Franco Lionel Rodriguez Caserta</Link> * 
           <Link href="https://www.linkedin.com/in/leslie-quetglas/" isExternal _hover={{color:"rgb(0, 255, 128)"}}> Leslie Quetglas</Link> *
           <Link href='https://www.linkedin.com/in/leilaaylensalguero/' isExternal _hover={{color:"rgb(0, 255, 128)"}}> Leila Salguero</Link> * 
           <Link href='https://www.linkedin.com/in/sebastian-concheso-83b095246/' isExternal _hover={{color:"rgb(0, 255, 128)"}}> Sebastián Concheso</Link> * 
@@ -28,12 +28,12 @@ const AboutUs = () => {
           <Link href='https://www.linkedin.com/in/julieta-gimenez-348172a5/' isExternal _hover={{color:"rgb(0, 255, 128)"}}> Julieta Gimenez</Link></b></Text>
       </Box>
       <Spacer />
-      <Box flex='1' p="5" borderWidth="5px" borderRadius="xl" bgColor="rgba(46, 48, 58, 0.8)">
+      <Box flex='1' p="5" borderWidth="4px" borderRadius="xl" bgColor="rgba(46, 48, 58, 0.8)">
         <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short" color={"white"}>What is NatureXtreme?</Text>
-        <Text mt={2} fontSize="md" color={"white"}>Naturaleza Xtreme is a project designed to exercise our use of tools like github and our skills for group work with agile methodologies.<br />Also, through this practice, we consolidate knowledge on tools used during the bootcamp and investigate and put into practice the use of new ones.</Text>
+        <Text mt={2} fontSize="md" color={"white"}>NaturXtreme is a project designed to exercise our use of tools like github and our skills for group work with agile methodologies.<br />Also, through this practice, we consolidate knowledge on tools used during the bootcamp and investigate and put into practice the use of new ones.</Text>
       </Box>
       <Spacer />
-      <Box flex='1' p="5" borderWidth="5px" borderRadius="xl" bgColor="rgba(46, 48, 58, 0.8)">
+      <Box flex='1' p="5" borderWidth="4px" borderRadius="xl" bgColor="rgba(46, 48, 58, 0.8)">
         <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short" color={"white"}>What technologies do we use?</Text>
         <Text mt={2} fontSize="md" color={"white"}><b>React * Redux * ChakraUI * Formik * TSNode * Express * TypeORM * Postgres * Nodemailer * MercadoPago * Cloudinary * Javascript * Typescript</b></Text>
       </Box>
