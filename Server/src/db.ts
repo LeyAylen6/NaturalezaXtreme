102c08f4a6a62ae753f32e7f837f20e8fcd905d6
Leila <leiisalguero@gmaill.com>
2023-07-05 13:00:19 -0300

    changes for deploy

--- Server/src/db.ts
+++ Server/src/db.ts
@@ -12,11 +12,11 @@ import { DataSource } from "typeorm"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: DB_HOST,
-    port: Number(DB_PORT),
-    username: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME_PROJECT,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_PROJECT,
     synchronize: true,
     logging: false,
     entities: [User, Article, Post, Shopping_Cart, Shopping_Cart_Article],
